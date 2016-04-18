80d9ce8e1c2896fd89c3dfe94e6dca0e66e9b8f2
Bill Nicholson <nicholdw@ucmail.uc.edu>
2016-04-17 22:45:00 -0400

    Another push

--- src/edu/uc/ucc/IT2045/nicholdw/Week14/Week14Topics.java
+++ src/edu/uc/ucc/IT2045/nicholdw/Week14/Week14Topics.java
@@ -6,16 +6,27 @@ security?
 coding conventions
 haiku
 java to stand-alone app
+enumerated data type
 **********************************/
 
 package edu.uc.ucc.IT2045.nicholdw.Week14;
 
 public class Week14Topics {
 
+	// An enumerated data type
+	private enum enumIceCream  {vanilla, chocolate, strawberry, coffee};
+	
 	public static void main(String[] arg) {
 		
-		// A classic recursion example: the factorial calculation
+		// A classic recursion example: the factorial calculation.
 		System.out.println("Factorial(10) = " + factorial(10));
+		
+		
+		// An enumerated data type is used to declare a variable
+		enumIceCream myIceCream;
+		myIceCream = enumIceCream.coffee;		// OK		
+//		myIceCream = 42;						// Not OK
+		System.out.println("My Ice Cream = " + myIceCream);
 		
 		
 	}
@@ -23,7 +34,7 @@ public class Week14Topics {
 	
 
 	/**
-	 * Compute the factorial of a number
+	 * Compute the factorial of a number using recursion.
 	 * @param num The number to be computed
 	 * @return The factorial of num
 	 */
